28dec3b3280390bac4492cd5eac3b33e7787ed10
peterlebedev <lebede.peter@gmail.com>
2018-11-23 16:53:41 +0100

    comment

--- mvpDagger/src/main/java/cz/lebedev/mvapp/di/MainActivityComponent.java
+++ mvpDagger/src/main/java/cz/lebedev/mvapp/di/MainActivityComponent.java
@@ -4,6 +4,7 @@ import cz.lebedev.mvapp.MainActivity;
 import dagger.Subcomponent;
 import dagger.android.AndroidInjector;
 
+//MainActivityModule poskytuje zavislosti pro danou aktivitu
 @Subcomponent(modules = {MainActivityModule.class})
 public interface MainActivityComponent extends AndroidInjector<MainActivity> {
     @Subcomponent.Builder
